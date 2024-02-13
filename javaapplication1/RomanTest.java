681e43e533a41bc746a98bfeef835c3dc0868d37
Tapiwa <tapiwachimbwanda4@gmail.com>
2024-02-13 15:06:27 -0500

    all tests in class running

--- javaapplication1/RomanTest.java
+++ javaapplication1/RomanTest.java
@@ -6,7 +6,7 @@ import org.junit.*;
 public class RomanTest {
 
     @Test
-    void singleDigit() {
+    public void singleDigit() {
         Assert.assertEquals(1, new Roman().romanToInt("I"));
         Assert.assertEquals(5, new Roman().romanToInt("V"));
         Assert.assertEquals(10, new Roman().romanToInt("X"));
@@ -17,15 +17,18 @@ public class RomanTest {
     }
 
     @Test
-    void repetition() {
+    public void repetition() {
         Assert.assertEquals(2, new Roman().romanToInt("II"));
         Assert.assertEquals(20, new Roman().romanToInt("XX"));
     }
 
+    /**
+     * 
+     */
     @Test
-    void manyLettersInOrder() {
-        Assert.assertEquals(1000, new Roman().romanToInt("VI"));
-        Assert.assertEquals(1000, new Roman().romanToInt("XV"));
+    public void manyLettersInOrder() {
+        Assert.assertEquals(6, new Roman().romanToInt("VI"));
+        Assert.assertEquals(15, new Roman().romanToInt("XV"));
     }
 
 }
